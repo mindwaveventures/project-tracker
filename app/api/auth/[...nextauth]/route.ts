4ecde797bda54dabccc1a40456021d8cc11de743
Selvakumar <selvakumar@mindwaveventures.com>
2024-09-26 16:17:13 +0530

    fix: session auth added with mongo

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,49 @@
 // imports
-import NextAuth from "next-auth";
+import { connectToMongoDB } from "@/app/lib/db";
+import UserModel from "@/app/model/users";
+import NextAuth, { AuthOptions } from "next-auth";
 
 // importing providers
 import GoogleProvider from "next-auth/providers/google";
 
-const handler = NextAuth({
+export const authOptions: AuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
-});
+  callbacks: {
+    async signIn({ user, account, profile }) {
+        
+      await connectToMongoDB(); // Connect to MongoDB
+
+      // Check if the user already exists
+      const existingUser = await UserModel.findOne({ email: user.email });
+
+      if (!existingUser) {
+        // Create a new user if they don't exist
+        const newUser = new UserModel({
+          name: user.name,
+          email: user.email,
+          image_url: user.image,
+        });
+        await newUser.save();
+      }
+
+      return true;
+    },
+    async session({ session, token, user }) {
+      // You can add extra fields to session here
+      return session; // Return the session object
+    },
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET, // Set your own secret
+};
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
--- app/api/role/route.ts
+++ app/api/role/route.ts
@@ -1,16 +1,8 @@
 import { connectToMongoDB } from "@/app/lib/db";
 import RoleModel from "@/app/model/roles";
-import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
-import { authOptions } from "../auth/[...nextauth]/route";
 
 export async function GET() {
-  const session = await getServerSession(authOptions);
-
-  if (!session) {
-    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-  }
-
   await connectToMongoDB();
   const ottApplications = await RoleModel.find({});
   return NextResponse.json({
